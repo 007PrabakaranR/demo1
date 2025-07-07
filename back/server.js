9f8ed78f5682e2363a88d26c3ec775113480760f
007PrabakaranR <prabakarandinesh05@gmail.com>
2025-07-07 17:56:17 +0530

    setup

---
+++ back/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load .env first
+
+const app = express(); // Define app BEFORE using it
+
+app.use(express.json());
+app.use(cors());
+
+const PORTZ = process.env.PORT || 4000; // fallback if .env is missing
+
+console.log("PORT from .env:", PORTZ);
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+
+app.listen(PORTZ, () => {
+  console.log(`✅ Server running on port: ${PORTZ}`);
+});
+
